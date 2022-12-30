bfd3e843aa4437a2b2ded310514da371a1f5e690
AhmedHodhod <alhdhody@gmail.com>
2022-12-30 19:26:55 +0200

    AllocateMemory is now working

--- phase1/memManager.c
+++ phase1/memManager.c
@@ -3,132 +3,90 @@
 #include "headers.h"
 #include "ml.c"
 
-
-int main()
+// 0 1 2 3 4  5  6  7   8   9   10
+// 1 2 4 8 16 32 64 128 256 512 1024
+void initializeMemoryManager(memList *free_list)
 {
-    // priQ **free_list = (priQ **)calloc(sizeof(priQ *), 11);
-
-    // add this line to the scheduler 
-    memList * free_list[11];
-
-    // seprate this into an initialize function 
+    // initialze each memory size list
     for (int i = 0; i < 11; i++)
     {
         memList mlist = (memList){
             .head = NULL,
             .tail = NULL,
-            .index = 3,
+            .index = i,
         };
-        free_list[i] = &mlist;  
+        free_list[i] = mlist;
     }
 
+    // initially, the whole memory is a hole [ 1024 bytes]
+    AddHole(&free_list[10], 0);
+}
 
-    // test with a process
-    process p;
-    p.id = 4;
-    p.arrival = 4;
-    p.runtime = 2;
-    p.remainingtime = 2;
-    p.priority = 0; 
-    p.memsize = 256;
-    p.memStart = 0;
-
-    
-    // initially, there is only one hole [add to initialize]
-    AddHole(free_list[10], 0); 
-   
-    
-    // use priQ of proccesses for now, consider priority = address
-
-    int list_index = ceil(log2(p.memsize));
+int AllocateMemory(memList *free_list, int memSize)
+{
+    // find the appropriate memory slot 
+    int list_index = ceil(log2(memSize));
     int memorySlot = pow(2, (int)list_index);
 
-    priQ *list = free_list[list_index];
     int i = list_index;
 
-    while (list->size == 0 && i < 11)
-    {
-        // printf("%d\n", i);
-        list = free_list[++i];
-    }
+    // find the least slot that suffices the memorySlot
+    while (GetMinAddress(&free_list[i]) == -1)
+        ++i;
+    
+    // if i > list_index, divide the larger slots 
 
-    // now list, holds the first available hole, list_index holds the index
-    // printf("log of 512: %d %d \n", list_index, i);
     int slot = pow(2, i);
-    process *m;
     int k = i;
+    int addr = -1;
+    //printf("got here\n");
     for (int j = 0; j < i - list_index; j++)
     {
-        // slot /= 2;
+        slot /= 2;
         // printf("slot to be divided : %d\n", slot);
-        m = priQpeek(free_list[k]);
-        priQremove(free_list[k], m);
-
+        addr = ExtractFirstAddress(&free_list[k]);
         // divide it into two equally sized blocks
 
-        // memStart is not correct [ due to using pointers ]
-        process ppp = (process){
-            .priority = m->memStart,
-            .memStart = m->memStart,
-        };
-        process pp = (process){
-            .priority = m->memStart + slot,
-            .memStart = m->memStart + slot,
-        };
-
-        priQenqueue(free_list[k - 1], &ppp);
-        priQenqueue(free_list[k - 1], &pp);
+        AddHole(&free_list[k - 1], addr);
+        AddHole(&free_list[k - 1], addr + slot);
         k--;
     }
-    // now, k holds the index of the block i want to use
+    
+    
+    // TEST: 
+    //display(&free_list[list_index]);
+    
+    addr= ExtractFirstAddress(&free_list[k]); 
+    return addr; 
+}
 
-    // allocate the kth block
-    m = priQpeek(free_list[k]);
-    priQremove(free_list[k], m);
-    p.memStart = m->priority; // priority represents the location
-    // becaue of the error in setting the starting time, set it manually here
+int main()
+{
+    // priQ **free_list = (priQ **)calloc(sizeof(priQ *), 11);
 
-    // deallocation algorithm
-    // assume the deallocated process keeps the size associated to it
-    // enqueue this process into the suitable list based on its memStart value [ address]
-    p.memStart = 256;
-    p.memsize = 256;
-    p.priority = 256;
+    // add this line to the scheduler
+    memList free_list[11];
 
-    // remove this when you have your list
-    m = priQpeek(free_list[8]);
-    m->memStart = 0;
-    m->memsize = 256;
-    m->priority = 0;
-    printf("first : %d\n", m->memStart);
+    initializeMemoryManager(free_list);
+   
 
-    // first, find the suitable block
+   // TEST: Allocate some memory slots
+    // for (int i =0; i < 4; i ++)
+    // {
+    //     int addr =  AllocateMemory(free_list, 256); 
+    //     printf("%d  %d\n", i, addr); 
+    // // addr represents the starting address of a slot = 256 [ assgn it to a process]
+    // }
+    
 
-    // remove the below line
-    process kk = (process){
-        .priority = 256,
-        .memsize = 256,
-        .memStart = 256,
-    };
+     // deallocation algorithm
 
-    priQenqueue(free_list[(int)log2(p.memsize)], &kk);
+    // // Now, you have two holes having 256[ 0->255->511]
 
-    // remove the below line
-    process kkk = (process){
-        .priority = 512,
-        .memsize = 512,
-        .memStart = 512,
-    };
+    // // m = priQpeek(free_list[(int)log2(p.memsize)]);
+    // // printf("second : %d\n", m->memStart);
 
-    priQenqueue(free_list[(int)log2(p.memsize)], &kkk);
+    // // Loop over the [(int)log2(p.memsize)]th  list and run the compaction algorithm
 
-    // Now, you have two holes having 256[ 0->255->511]
-
-    // m = priQpeek(free_list[(int)log2(p.memsize)]);
-    // printf("second : %d\n", m->memStart);
-
-    // Loop over the [(int)log2(p.memsize)]th  list and run the compaction algorithm
-
-
-        return 0;
-    }
+    return 0;
+}
